0304376c2102f8e0d4dbaa7d02791f1e58d28924
Lokesh <109655393+rathorel24@users.noreply.github.com>
2022-09-04 15:31:02 +0530

    Update settings.py

--- SalesArea/settings.py
+++ SalesArea/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$p#m-ja7iq@2+**yj-b674q3klu@^p9v&!$yep2^=xu4rfa&$)'
+SECRET_KEY = ${{ env.MY_KEY }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
